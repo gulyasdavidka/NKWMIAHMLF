7ad275f6fa55b3f746c7df79fd21d1aef3062229
gulyasdavidka <gulyasdavid1997@gmail.com>
2025-11-03 18:32:17 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Division by zero!")
+        return a / b
